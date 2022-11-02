1ef4a99c0da1c84e30c82e06bf0f035c54b800a3
Charlie Van Norman <charlie@vannorman.ai>
2022-11-02 03:21:19 +0000

    first

---
+++ stub/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+def main(request):
+    """View function for home page of site."""
+
+    context = {
+#        'num_books': num_books,
+#        'num_instances': num_instances,
+#        'num_instances_available': num_instances_available,
+#        'num_authors': num_authors,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
+    
+
