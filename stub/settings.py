9ac1e48f885f9de6224322edc71f4cb0a01f7742
Charlie Van Norman <charlie@vannorman.ai>
2022-12-08 09:41:39 -0600

    working local server. added faq. added schedule. css stuff

--- stub/settings.py
+++ stub/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'k#jsm)@j%hrjg7b+%k6f&13ne$mk=u0m==u#i_y9lmz-s(f+cb'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['143.198.76.172','www.mana.house','mana.house']
+ALLOWED_HOSTS = ['143.198.76.172','www.mana.house','mana.house','127.0.0.1']
 
 
 # Application definition
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'stub.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
@@ -124,6 +124,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+STATICFILES_DIRS = (
+  os.path.join(BASE_DIR, 'static'),
+  '/Users/ccnorman/websites/manahouse/static/',
+)
+APPEND_SLASH = False
+
--- stub/views.py
+++ stub/views.py
@@ -3,12 +3,22 @@ def main(request):
     """View function for home page of site."""
 
     context = {
+        'faq' : [
+            {
+                'q' : 'quesiton1',
+                'a' : 'question2',
+             },
+            {
+                'q' : 'quesiton1',
+                'a' : 'question2',
+             },
+        ]
 #        'num_books': num_books,
 #        'num_instances': num_instances,
 #        'num_instances_available': num_instances_available,
 #        'num_authors': num_authors,
     }
-
+    
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
     
