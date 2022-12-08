4cf5bdc4383ea858065f91efdfe5159eb7df7829
Charlie Van Norman <charlie@vannorman.ai>
2022-12-08 14:16:46 +0000

    server changes

--- stub/settings.py
+++ stub/settings.py
@@ -16,6 +16,7 @@ import os.path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent
 
+print('base dir:'+str(BASE_DIR))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
